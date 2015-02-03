ad287e56f71b384c48db6407a44d95947e8a2994
keertishukla <shukla.keerti@gmail.com>
2015-02-03 16:31:26 -0500

    area and sphere

---
+++ area.py
@@ -0,0 +1,12 @@
+##############################
+# Title: Area of a Rectangle #
+# Filename: area.py          #
+# 
+
+width = 20                          #Length, meters
+height = 30                         #Length, meters
+
+area = width*height                 #Area, m^2
+print area
+
+print area
---
+++ sphere.py
@@ -0,0 +1,7 @@
+from __future__ import division, print_function
+from visual import *
+charge = sphere ()
+charge.radius = 1
+charge.pos = vector(1, 0, 0)
+charge.color = (0.5, 1, 0.5)
+charge2 = sphere(pos = vector(1, 0, 0), radius = 2, color = color.red, opacity = 0.5)
